108a6b4ea2d7931905f0ee988e6790b8f0a50285
FuryDev <33258743+TheFuryBumblebee@users.noreply.github.com>
2022-11-18 12:02:51 -0800

    Add test extension

---
+++ test.js
@@ -0,0 +1,21 @@
+class TestExtension {
+  getInfo() {
+    return {
+      id: 'test-extension',
+      name: 'Test Extension',
+      blocks: [
+        {
+          opcode: 'pi',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'π'
+        }
+      ]
+    }
+  }
+  
+  pi() {
+    return Math.PI;
+  }
+}
+
+Scratch.extensions.register(new TestExtension());
